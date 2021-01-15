2ffd2e5099fedfa49f63169381fd824a6703a3bc
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 16:03:20 +0100

    Update exo_06.rb

--- exo_06.rb
+++ exo_06.rb
@@ -1,15 +1,13 @@
 
+puts "écrit un nombres allez est que sa saute"
 
-        puts "écrit un nombres allez est que sa saute"
+nombre = gets.chomp.to_i
 
-        nombre = gets.chomp.to_i
-        
-        puts "ton nombre ne te portera pas chance et oue: #{nombre} "
-        
-        
-        13.times do
-            puts "La malchance arrive regarde: Salut, ça farte ?" 
-        end    
-        
+nombre.times do
+
+      puts "Salut, ça farte ? "
+end    
+
+      
 
 
