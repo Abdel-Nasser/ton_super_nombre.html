05ba36c0793dc4cfa4499ef8f29531ac7e5bc4f0
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 18:51:09 +0100

    Update exo_11.rb

--- exo_11.rb
+++ exo_11.rb
@@ -1,16 +1,17 @@
         
 
+=begin
+Notre programme exo_12.rb est devenu beau gosse. Écris un programme exo_17.rb qui va faire la même chose,
+ sauf que si X et Y sont égaux, il dira "Il y a n ans, tu avais la moitié de l'âge que tu as aujourd'hui".
+=end
+
+
+
 puts "quelle age as tu!"
+print ">"
 age= gets.chomp.to_i
 
+age  = age / 2 
+année = 2020 - age
 
-for age in 1..age 
-     puts "votre age: #{age} ans"
-     
-     
-end
-
-while 2021 == age 
-         puts  "année de naissance #{age}"   
-      age = age - 1
-     end 
+puts "en #{année} tu avait #{age}"
