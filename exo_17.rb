ac50a97ccc5ed7a580ceafbde6d4831a4239b4e4
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 20:35:56 +0100

    Update exo_17.rb

--- exo_17.rb
+++ exo_17.rb
@@ -1,1 +1,40 @@
 
+=begin
+    
+    2.17. La pyramide, version expert
+Crée un programme exo_17.rb qui va demander à l'utilisateur un nombre entre 1 et 25 et qui va sortir une pyramide qui monte et qui descend, comme montré ci-dessous :
+
+=end
+
+
+
+
+
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print ">"
+nb = gets.chomp.to_i
+
+print "voici la pyramide!"
+
+nb.times do |i|
+          
+    puts     " " - nb   + "#" * i
+    i = i + 1
+    nb = nb - 1
+   
+
+
+
+       if 25 < nb && 0 > 25
+
+       
+        puts  "desoler vous ne pouvez monter de plus de 5 étages, au revoir!"
+
+       
+
+         end
+       
+
+  
+     end
