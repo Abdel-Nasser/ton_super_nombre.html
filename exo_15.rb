10c15e326feef993849ef9ce868c192fb03cceee
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 20:11:34 +0100

    Update exo_15.rb

--- exo_15.rb
+++ exo_15.rb
@@ -10,3 +10,40 @@ else
     puts
   end
 end  
+
+=begin
+
+Construis un programme exo_15.rb qui va demander à l'utilisateur un nombre entre 1 et 25
+ et qui va sortir une pyramide à descendre d'autant d'étages que ce nombre. Voici un exemple :
+
+=end
+
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print ">"
+nb = gets.chomp.to_i
+
+print "voici la pyramide!"
+
+nb.times do |i|
+          
+       puts  "#" * i
+       i = i + 1
+       nb = nb + 1
+
+       if 25 < nb && 0 > 25
+
+       
+        puts  "desoler vous ne pouvez monter de plus de 5 étages, au revoir!"
+
+        nb = gets.chomp.to_i
+
+         end
+       
+
+  
+     end
+
+
+
+
