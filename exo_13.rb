262c8db5fac7611c9683a405f227cd61ed104fe2
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 19:13:21 +0100

    Update exo_13.rb

--- exo_13.rb
+++ exo_13.rb
@@ -1,8 +1,15 @@
 
-email = gets.to_i
+=begin
 
-for chrono in 1..50 do
+2.13. Une liste d'email
+Écris un programme exo_13.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails :
 
-    puts "jean.dupont.#{chrono}@email.fr"
-    
+=end
+
+
+mail = ['"jean.dupont.01@email.fr"'] 
+
+
+50.times do |i|
+     puts  mail
 end
