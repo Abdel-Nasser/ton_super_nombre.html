aee264fa26007f3512d0aab00b4aaa8869ac53f1
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 16:10:14 +0100

    Update exo_08.rb

--- exo_08.rb
+++ exo_08.rb
@@ -2,7 +2,9 @@ puts "encore écrit un chiffre"
        print "_"
 
        nombre = gets.chomp.to_i
+       nombre = nombre + 1
        
        nombre.times  do |i|
            puts i  
+
        end
