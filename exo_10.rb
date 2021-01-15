341dc11008ad62bf30cff61e535b726394796210
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 17:25:51 +0100

    Update exo_10.rb

--- exo_10.rb
+++ exo_10.rb
@@ -12,3 +12,62 @@ puts " écrit ton année de naissance pour voir si t'ai pas un pépé"
             years_born = years_born + 1
             depart_years = depart_years + 1
           end
+
+
+
+
+=begin 
+
+2.10. Afficher tous les âges
+Écris un programme exo_10.rb qui demande son année de naissance à l'utilisateur et qui va afficher chaque année depuis son année de naissance jusqu'aujourd'hui.
+ Pour chaque année affichée, le programme devra annoncer l'âge que l'utilisateur avait cette année-là.
+=end
+
+puts " écrit ton année de naissance pour voir si t'ai pas un pépé"
+          print ">"
+          years_born = gets.chomp.to_i
+          depart_years = 0
+         
+=begin          for n in years_born..2020
+              age = age + 1
+
+          end
+          
+=end
+
+     while years_born <= 2020
+         print "ton année de naissance!"
+         puts  years_born
+        years_born = years_born + 1  
+     end
+
+
+          while  years_born==2020  && depart_years   do  
+
+              print "en: "
+               puts years_born
+
+            years_born = years_born + 1
+
+          print  "tu avais " 
+          puts depart_years
+
+          depart_years = 
+
+              
+          end
+       
+   1
+                  end
+      
+
+
+
+
+=begin 
+
+2.10. Afficher tous les âges
+Écris un programme exo_10.rb qui demande son année de naissance à l'utilisateur et qui va afficher chaque année depuis son année de naissance jusqu'aujourd'hui.
+ Pour chaque année affichée, le programme devra annoncer l'âge que l'utilisateur avait cette année-là.
+=end
+    
