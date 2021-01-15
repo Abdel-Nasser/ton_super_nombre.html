9b98e37e72c8f07e2e95cbdbd8cf11a1c5389890
Abdel-Nasser <77011658+Abdel-Nasser@users.noreply.github.com>
2021-01-15 16:07:31 +0100

    Update exo_07.rb

--- exo_07.rb
+++ exo_07.rb
@@ -1,9 +1,10 @@
 puts "bon maintenant tu va mettre un chiffre et je vais te faire une blague"
 
    nombre = gets.chomp.to_i
-   nombre -= 1
    
    
+       nombre = nombre - 1
+
  nombre.times do 
     puts "bonjour bien!" 
   end
